a62b876a2fb7fe6333ae252f82d3a5ac70c4e9f5
Justin Tokarchuk <jtokarchuk@outlook.com>
2023-02-03 16:25:38 -0600

    added some barebones emulation logic

--- src/main.c
+++ src/main.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include "cpu.h"
 
 bool application_closing = false;
 
 
 int main(int argc, char *argv[]) {
     printf("PowerEmu Initializing...\n");
+    cpu_reset();
+    // load GB BIOS
+    // load a ROM
 
     while( !application_closing ) {
-
+        cpu_emulate();
+        // gpu_emulate()
+        // sound_emulate() ?
+        // interrupt_emulate()
     }
     
     printf("PowerEmu Shutting Down");
